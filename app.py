3301e3731a25a213abc71fe1f7a911bb4022ba69
noelfonsecairri <n.fonseca@irri.org>
2020-02-04 18:12:05 +0800

    added app.route /json_example

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, json
+from flask import Flask, Response, json, request
 
 app = Flask(__name__)
 
@@ -8,9 +8,42 @@ def index():
 
 @app.route('/user', methods=['GET'])
 def user():
-	resp_dict = {"first_name": "John", "last_name": "Doe"}
+	resp_dict = {"first_name": "Noel", "last_name": "Fonseca"}
 	response = Response(json.dumps(resp_dict),200)
 	return response
+
+@app.route('/notifications', methods=['POST', 'GET'])
+def notifications():
+	if request.method == 'POST':
+		language = request.form.get('language')
+		framework = request.form['framework']
+
+
+		return '''<h1>The language value is: {}</h1>
+				<h1>The framework value is {}</h1>'''.format(language, framework)
+
+
+	return '''<form method="POST">
+		Language: <input type="text" name="language"><br>
+		Framework: <input type="text" name="framework"><br>
+		<input type="submit" value="Submit"><br>
+	</form>'''
+
+@app.route('/json_example', methods=['POST'])
+def json_example():
+	req_data = request.get_json()
+
+	language = req_data['language']
+	framework = req_data['framework']
+	website = req_data['website']
+
+
+	return '''<h1>
+	The language value is {}.
+	The framework value is {}.
+	The website value is {}.
+	</h1>'''.format(language, framework, website)
+
 
 if __name__ == "__main__":
 	app.run()
