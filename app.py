cc56f111e7719f6dabf8e820601b3ed974274298
noelfonsecairri <n.fonseca@irri.org>
2020-02-07 17:02:24 +0800

    changed the receiving url to /notifications

--- app.py
+++ app.py
@@ -12,25 +12,8 @@ def user():
 	response = Response(json.dumps(resp_dict),200)
 	return response
 
-@app.route('/notifications', methods=['POST', 'GET'])
+@app.route('/notifications', methods=['POST'])
 def notifications():
-	if request.method == 'POST':
-		language = request.form.get('language')
-		framework = request.form['framework']
-
-
-		return '''<h1>The language value is: {}</h1>
-				<h1>The framework value is {}</h1>'''.format(language, framework)
-
-
-	return '''<form method="POST">
-		Language: <input type="text" name="language"><br>
-		Framework: <input type="text" name="framework"><br>
-		<input type="submit" value="Submit"><br>
-	</form>'''
-
-@app.route('/json_example', methods=['POST'])
-def json_example():
 	req_data = request.get_json()
 
 	language = req_data['language']
